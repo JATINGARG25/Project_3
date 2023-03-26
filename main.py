370732066fb9f5908260aae39286c7086986a4c5
JATINGARG25 <116997476+JATINGARG25@users.noreply.github.com>
2023-03-26 10:46:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+class Library:
+    def __init__(self,listofbooks):
+        self.books = listofbooks
+
+    def displayavailablebooks(self):
+        print("\nBooks present in this library are:\n")
+        for book in self.books:
+            print("* " + book)
+
+    def borrowbook(self,bookname):
+        if bookname in self.books:
+            print(f"\nYou issued book {bookname}. Please keep it safe and return within 30 days.")
+            self.books.remove(bookname)
+            return True
+        else:
+            print("\nThis book is either not available or has been already issued to someone else. Please wait until the book is available.")
+            return False
+        
+    def returnbook(self,bookname):
+        self.books.append(bookname)
+        print("\nThanks for returning the book.")
+
+class Student:
+    def requestbook(self):
+        self.book = input("\nEnter the name of the book you want to borrow: ")
+        return self.book
+    def returnbook(self):
+        self.book = input("\nEnter the name of the book you want to return: ")
+        return self.book
+
+if __name__ == "__main__":
+    student = Student()
+    central_library = Library(["Algorithms","Django","Full stack","Python notes","C notes"])
+    while(True):
+
+        wlcmsg = '''\n****Welcome to central library****
+
+    Please choose an option:
+    1. Listing all the books
+    2. Request a book
+    3. Add or Return a book
+    4. Exit the library
+    '''
+        print(wlcmsg)
+
+        a = int(input("Enter a choice: "))
+        if a==1:
+            central_library.displayavailablebooks()
+        elif a==2:
+            central_library.borrowbook(student.requestbook())
+        elif a==3:
+            central_library.returnbook(student.returnbook())
+        elif a==4:
+            print("\nThanks for choosing central library.")
+            exit()
+        else:
+            print("\nInvalid choice")
